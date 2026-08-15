604a94ff92d90442a3555e4840b688b00d41c6b4
isabelaveloso81 <isabelaveloso57@gmail.com>
2026-08-15 08:07:40 -0300

    Create Exercicio2.java

---
+++ Exercicio2.java
@@ -0,0 +1,7 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Primeira mensagem");
+        System.out.println("Segunda mensagem");
+        System.out.println("Terceira mensagem");
+    }
+}
