04963cbd0f2f4c144ec81d1c89ed37f0a05c4558
isabelaveloso81 <isabelaveloso57@gmail.com>
2026-08-15 08:00:57 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nome: João Silva");
+        System.out.println("Curso: Ciência da Computação");
+    }
+}
