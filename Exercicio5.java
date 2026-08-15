a806621a8177ca022a018acf143c877b45c2622d
isabelaveloso81 <isabelaveloso57@gmail.com>
2026-08-15 08:26:50 -0300

    Exercicio5.java

---
+++ Exercicio5.java
@@ -0,0 +1,11 @@
+public class Exercicio5 {
+    public static void main(String[] args) {
+        String nome = "Maria";
+        int idade = 20;
+        double altura = 1.65;
+        double nota = 9.5;
+        boolean matriculado = true;
+
+        System.out.println("Nome: " + nome + " | Idade: " + idade + " | Altura: " + altura + " | Nota: " + nota + " | Matriculado: " + matriculado);
+    }
+}
