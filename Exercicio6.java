bf1175ae272ecbc15b34234ec561dfefd26d82cc
isabelaveloso81 <isabelaveloso57@gmail.com>
2026-08-15 09:17:09 -0300

    Create Exercicio6.java

---
+++ Exercicio6.java
@@ -0,0 +1,7 @@
+public class Exercicio6 {
+    public static void main(String[] args) {
+        String nome = "Marina";
+        int idade = 19;
+        System.out.println(nome + " possui " + idade + " anos.");
+    }
+}
