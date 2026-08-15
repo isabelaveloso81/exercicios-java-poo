53aeaec69f419678959981607ddd452b5aee47dc
isabelaveloso81 <isabelaveloso57@gmail.com>
2026-08-15 08:09:19 -0300

    Create Exercicio3.java

---
+++ Exercicio3.java
@@ -0,0 +1,8 @@
+public class Exercicio3 {
+    public static void main(String[] args) {
+        System.out.println("FICHA DO ALUNO");
+        System.out.println("Nome: Maria");
+        System.out.println("Curso: Ciência da Computação");
+        System.out.println("Turma: 1º Semestre");
+    }
+}
