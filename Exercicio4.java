3fef8a578046d656cc9f5fa6137b42b32664fcf2
isabelaveloso81 <isabelaveloso57@gmail.com>
2026-09-24 22:27:16 -0300

    Create Exercicio4.java

---
+++ Exercicio4.java
@@ -0,0 +1,11 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        System.out.print("Olá ");
+        System.out.print("Java");
+
+        System.out.println();
+
+        System.out.println("Olá");
+        System.out.println("Java");
+    }
+}
