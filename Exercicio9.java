de22301b57acc3b2863e1de2ccb076bc03b7f1ae
isabelaveloso81 <isabelaveloso57@gmail.com>
2026-09-24 23:09:34 -0300

    Exercicio9.java

---
+++ Exercicio9.java
@@ -0,0 +1,12 @@
+public class Exercicio9 {
+    public static void main(String[] args) {
+        double A = 20;
+        double B = 6;
+
+        System.out.println("Soma: " + (A + B));
+        System.out.println("Subtração: " + (A - B));
+        System.out.println("Multiplicação: " + (A * B));
+        System.out.println("Divisão: " + (A / B));
+        System.out.println("Resto: " + (A % B));
+    }
+}
