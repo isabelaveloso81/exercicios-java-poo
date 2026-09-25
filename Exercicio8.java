5d06cfed9336be833e97b545c4571d4d89f6cbd8
isabelaveloso81 <isabelaveloso57@gmail.com>
2026-09-24 22:37:19 -0300

    Exercicio8.java

---
+++ Exercicio8.java
@@ -0,0 +1,13 @@
+public class Exercicio8 {
+    public static void main(String[] args) {
+        int idade = 20;
+        double altura = 1.75;
+        char letra = 'A';
+        boolean matriculado = true;
+
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(letra);
+        System.out.println(matriculado);
+    }
+}
