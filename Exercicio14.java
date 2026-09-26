742675c9c317068d8c3b2e8ede73681957c56839
isabelaveloso81 <isabelaveloso57@gmail.com>
2026-09-26 06:07:45 -0300

    Exercicio14.java

---
+++ Exercicio14.java
@@ -0,0 +1,10 @@
+public class Exercicio14 {
+    public static void main(String[] args) {
+        double preco = 35.50;
+        double valorPago = 50.00;
+
+        double troco = valorPago - preco;
+
+        System.out.println("Troco: R$ " + troco);
+    }
+}
