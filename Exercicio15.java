ee79d7e0c82f992d3a301e8891205c62ace904dc
isabelaveloso81 <isabelaveloso57@gmail.com>
2026-09-26 06:08:32 -0300

    Exercicio15.java

---
+++ Exercicio15.java
@@ -0,0 +1,10 @@
+public class Exercicio15 {
+    public static void main(String[] args) {
+        double distancia = 300;
+        double combustivel = 25;
+
+        double consumoMedio = distancia / combustivel;
+
+        System.out.println("Consumo médio: " + consumoMedio + " km/l");
+    }
+}
